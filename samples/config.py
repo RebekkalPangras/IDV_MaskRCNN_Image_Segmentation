6a632a03edb471cb06f0dbdff59434ee16b94b4d
Rebekkal <rebeccapancras@gmail.com>
2019-08-06 11:42:59 +0530

    Modified for collab

--- samples/config.py
+++ samples/config.py
@@ -1,12 +1,16 @@
 import os
 import sys
-import json
-import datetime
+import time
 import numpy as np
-import skimage.draw
+import imgaug  # https://github.com/aleju/imgaug (pip3 install imgaug)
+
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+from pycocotools import mask as maskUtils
+
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
+ROOT_DIR = "/content/IDV_MaskRCNN_Image_Segmentation"
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -14,7 +18,7 @@ from mrcnn.config import Config
 from mrcnn import model as modellib, utils
 
 # Path to trained weights file
-COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+MICR_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
 # Directory to save logs and model checkpoints, if not provided
 # through the command line argument --logs
@@ -24,23 +28,25 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 #  Configurations
 ############################################################
 
-
-class IDVConfig(Config):
-    """Configuration for training on the toy  dataset.
+class MicrConfig(Config):
+    """Configuration for training on MICR.
     Derives from the base Config class and overrides some values.
     """
     # Give the configuration a recognizable name
-    NAME = "idv"
+    NAME = "micr"
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
     IMAGES_PER_GPU = 2
 
+    # Uncomment to train on 8 GPUs (default is 1)
+    # GPU_COUNT = 8
+
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 3  # Background + balloon
+    NUM_CLASSES = 1 + 5  # micr has 80 classes
 
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 20
+    #STEPS_PER_EPOCH = 100
 
-    # Skip detections with < 60% confidence
-    DETECTION_MIN_CONFIDENCE = 0.6
+    # Skip detections with < 90% confidence
+#    DETECTION_MIN_CONFIDENCE = 0.9
