cb8cf1c0f42267d7b22c6d6a390e3f5a69908920
Rebekkal <rebeccapancras@gmail.com>
2019-08-06 11:58:47 +0530

    Package import changed

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ The build/compilations setup
 import pip
 import logging
 import pkg_resources
-from IDV_MaskRCNN_Image_Segmentation import mrcnn
+import mrcnn
 try:
     from setuptools import setup
 except ImportError:
